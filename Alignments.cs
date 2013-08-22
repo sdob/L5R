9040fdec6132f0b397768b91d5a5fc5fc7cb00d4
sdob <sdob@cs.ucc.ie>
2013-08-22 12:08:30 +0100

    Added clan alignments

---
+++ Alignments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace L5R {
+    public class Alignments {
+        public class Alignment {
+            readonly string name;
+            public Alignment(string name) {
+                this.name = name;
+            }
+        }
+        // TODO: add the rest of the possible clan alignments
+        public static Alignment Mantis = new Alignment("Mantis");
+        public static Alignment Unaligned = new Alignment("Unaligned");
+    }
+}
